27c7716f01ee5973c1787f988e2840370bd5665b
Mohamed Metwalli <mohamedmetwalli5@gmail.com>
2021-01-11 21:14:51 +0200

    r
    r

--- ProducerConsumerSimulator/src/TheProgram/Main.java
+++ ProducerConsumerSimulator/src/TheProgram/Main.java
@@ -1,10 +1,5 @@
 package TheProgram;
 
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
 public class Main {
 
 	public static void main(String[] args) {
